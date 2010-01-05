ed83ebd96cb61ab250fd23a86f11ac30adedfbbc
Marc S. Brooks <mbrooks@cpan.org>
2010-01-05 09:38:03 +0000

    Import of MBROOKS/Apache2-WebApp-Plugin-Mail from CPAN.
    gitpan-cpan-distribution: Apache2-WebApp-Plugin-Mail
    gitpan-cpan-version:      
    gitpan-cpan-path:         MBROOKS/Apache2-WebApp-Plugin-Mail.tar.gz
    gitpan-cpan-author:       MBROOKS
    gitpan-cpan-maturity:     released

--- lib/Apache2/WebApp/Plugin/Mail.pm
+++ lib/Apache2/WebApp/Plugin/Mail.pm
@@ -21,7 +21,7 @@ use MIME::Lite::TT;
 use MIME::Lite::TT::HTML;
 use Params::Validate qw( :all );
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~[  OBJECT METHODS  ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
